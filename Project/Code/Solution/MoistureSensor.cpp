f95c4cc52aaf8159de5cbbd0532b398709432281
Christian Lundtofte <christian@imakezappz.com>
2015-11-04 11:00:10 +0100

    as

--- Project/Code/Solution/MoistureSensor.cpp
+++ Project/Code/Solution/MoistureSensor.cpp
@@ -1,11 +1,42 @@
 #include "MoistureSensor.h"
 
-MoistureSensor::MoistureSensor(int aPin) {
+MoistureSensor::MoistureSensor(int aPin) 
+{
     myPin = aPin;
 }
 
+int middle(int v1, int v2, int v3)
+{
+    if(v1 <= v2 && v2 <= v3)
+    {
+        return v2;
+    }
+    if(v2 <= v1 && v1 <= v3)
+    {
+        return v1;
+    }
+    if(v1 <= v3 && v3 <= v2)
+    {
+        return v3;
+    }
+    
+    return -1;
+}
+
 // Implementering af read ved moisture sensor
+<<<<<<< Updated upstream
 int MoistureSensor::read() {
     int val =  1337; // analogRead(myPin);
     return val;
 }
+=======
+int MoistureSensor::read() 
+{
+    // Read three values and return the middle value
+    int val1 = 1337; // analogRead(myPin);
+    int val2 = 1338; // analogRead(myPin);
+    int val3 = 1339; // analogRead(myPin);
+    
+    return middle(val1, val2, val3);
+}
+>>>>>>> Stashed changes
