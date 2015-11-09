9bc267221a01caef915d0371bfbe26fc4743fb37
Cleevil <Cleevil@users.noreply.github.com>
2015-11-09 12:00:57 +0100

    Main and PacketHandler "merged"

--- Project/Code/Solution/Main.cpp
+++ Project/Code/Solution/Main.cpp
@@ -3,12 +3,91 @@
 #include "MoistureSensor.h" // Burde jo .h   men altså.. Det virker!
 //#import "Packet.h"
 //#import "PacketHandler.h"
+//#include "RadioHandler.h"
+
+//Put This in header?
+private:
+    bool _waitForAcknowledgement;
+    bool _readyToForward;
+    RadioHandler _radioHandler;
 
 int main(int argc, char *argv[])
 {
+    _waitForAcknowledgement = true;
+    _readyToForward = false;
+    
     MoistureSensor sensor(1);
     
     std::cout << "Main is running! \\o/" <<  std::endl << "Sensor value: " <<  sensor.read() << std::endl;
     
     return 0;
+}
+
+    // FROM PACKETHANDLER //
+void handlePacket(Packet packet)
+{
+    switch(packet.type);
+    {
+        case Acknowledgement :
+        {
+            if (_waitForAcknowledgement)
+            {
+                _waitForAcknowledgement = false;
+                _readyToForward = true;
+            }
+        }
+        break;
+        case Request :
+        {
+            if (!_waitForAcknowledgement && !_readyToForward)
+            {
+                _waitForAcknowledgement = true;
+                readPackSend();
+            }
+        }
+        break;
+        case Data :
+        {
+            if (_readyToForward)
+            {
+                forwardSignal(packet);
+            }
+        }
+        break;
+        case PairRequest :
+        {
+            // Pair me up Scotty!
+        }
+        break;
+        case PairRequestAcknowledgement :
+        {
+            // Ur paired dood
+        }
+        break;
+        case ClearSignal :
+        {
+            // I had nothing to do with it!
+        }
+        break;
+        default:
+            std::cout << "Hello? Yes, this is default.";
+        break;
+    }
+}
+
+void readPackSend()
+{
+    int sensorData = sensor -> read();              // Read
+    
+    Packet packet = new Packet(sensorData);         // Pack
+    
+    _radioHandler -> broadcast(packet->toString()); // Send
+    _radioHandler -> waitForAccept();
+}
+
+void forwardSignal(Packet packet)
+{
+    // Burde dette ligge i radioHandler?
+    _radioHandler -> broadcast(packet->toString());
+    // waitForAccept() ?
 }
