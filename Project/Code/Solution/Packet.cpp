d58c98fd828a94e23998ad256470e3cd86e8396f
helisium <helisium@hotmail.com>
2015-11-09 09:04:57 +0100

    Packet-klassen uden checksum

--- Project/Code/Solution/Packet.cpp
+++ Project/Code/Solution/Packet.cpp
@@ -1,6 +1,50 @@
 #include "Packet.h"
 
-Packet::Packet(int sensorData)
-{
-  
-}
+	Packet::Packet(string input)
+	{
+		decode(input);
+	}
+
+	Packet::Packet(PacketType packetTypeInput, uint16_t addresserInput, uint16_t addresseeInput, uint16_t originInput, uint16_t sensor1Input,
+		uint16_t sensor2Input, uint16_t sensor3Input)
+	{
+		this->packetType = packetTypeInput;
+		this->addresser = addresserInput;
+		this->addressee = addresseeInput;
+		this->origin = originInput;
+		this->sensor1 = sensor1Input;
+		this->sensor2 = sensor2Input;
+		this->sensor3 = sensor3Input;
+		this->checksum = getChecksum();
+	}
+
+
+	string Packet::encode(){
+
+		char returnstring[sizeof(Packet)-1];
+
+		memcpy(returnstring, this, sizeof(Packet)-1);
+
+		string mystring = string(returnstring, returnstring + sizeof(Packet)-1);
+
+		return mystring;
+	}
+
+
+private:
+	void Packet::decode(string input)
+	{
+		if (input.length() == 15)
+		{
+			memcpy(this, input.c_str(), sizeof(Packet)-1);
+		}
+		else
+		{
+			throw new exception("String not accepted for decoding");
+		}
+	}
+
+	uint16_t Packet::getChecksum()
+	{
+		return 65535;
+	}
--- Project/Code/Solution/Packet.h
+++ Project/Code/Solution/Packet.h
@@ -1,17 +1,28 @@
-enum PacketType
-{
-  Acknowledgement,
-  Request,
-  Data,
-  PairRequest,
-  PairRequestAcknowledgement,
-  ClearSignal
+enum PacketType : uint8_t {
+	Acknowledgement,
+	Request,
+	Data,
+	PairRequest,
+	PairRequestAcknowledgement,
+	ClearSignal
 };
 
 class Packet
 {
 public:
-    PacketType type;
-    
-    Packet(int);
+	uint16_t addresser;
+	uint16_t addressee;
+	uint16_t origin;
+	uint16_t sensor1;
+	uint16_t sensor2;
+	uint16_t sensor3;
+	uint16_t checksum;
+	PacketType packetType;
+	Packet(string);
+	Packet(PacketType, uint16_t, uint16_t, uint16_t, uint16_t, uint16_t, uint16_t);
+	string encode();
+
+private:
+	void decode(string);
+	uint16_t getChecksum();
 };
