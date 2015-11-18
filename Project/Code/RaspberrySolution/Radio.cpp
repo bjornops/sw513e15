28b47eab4f210e0d11c4148f614721990416ca62
Blezzing <morten@Blezzing>
2015-11-18 10:54:40 +0100

    Raspberry solution added.

---
+++ Project/Code/RaspberrySolution/Radio.cpp
@@ -0,0 +1,65 @@
+#include "Radio.h"
+
+//Sætter hardware op.
+NRF24Radio::NRF24Radio(int cePin, int csPin) 
+{ 
+    _radio = new RF24(cePin, csPin);
+    _radio->begin();      
+    _radio->setAutoAck(false);
+    _radio->setDataRate(RF24_250KBPS);
+    _radio->setPALevel(RF24_PA_MIN);
+    _radio->setCRCLength(RF24_CRC_DISABLED);
+    _radio->setChannel(_channel);
+    _radio->openReadingPipe(_readingPipe, _rxAddr);
+    
+    _radio->startListening();
+}  
+
+// Sender pakke ud som string.
+void NRF24Radio::broadcast(char *packetAsString)
+{
+    _radio->stopListening();
+    _radio->write(&packetAsString, strlen(packetAsString));
+}
+
+// Lyt indtil en pakke modtages, eller tiden er gået
+char *NRF24Radio::listenFor(unsigned long ms)
+{
+    _radio->startListening();
+    unsigned long firstMs = millis();
+    
+    while(true)
+    {
+        if(_radio->available()) // Læs og returner data
+        {
+            memset(lastMessage, 0, 32);
+            _radio->read(&lastMessage, 32*sizeof(char));
+            
+            return lastMessage;
+        }
+        
+        // Er tiden gået?
+        unsigned long thisMs = millis();
+        if(thisMs-firstMs > ms)
+        {
+            return "";
+        }
+    }
+}
+
+// Lyt indtil en pakke er klar (Blokerer)
+char *NRF24Radio::listen(void)
+{
+    _radio->startListening();
+    
+    while(true)
+    {
+        if (_radio->available()) // Læs og returner data
+        {
+            memset(lastMessage, 0, 32);
+            _radio->read(&lastMessage, 32*sizeof(char));
+            
+            return lastMessage;
+        }
+    }
+}
---
+++ Project/Code/RaspberrySolution/Radio.h
@@ -0,0 +1,46 @@
+#include "iRadio.h"
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#ifndef RADIO_H
+#define RADIO_H
+
+class NRF24Radio : public iRadio
+{
+private:
+    RF24 *_radio; 
+    
+    const uint64_t _rxAddr = 0xF0F0F0F0E1LL;
+    const int _channel = 114;
+    const int _readingPipe = 0;
+    
+    //UNO
+    /*
+    const int _cePin = 7;
+    const int _csPin = 8;
+    */
+    //MEGA 
+    /*
+    const int _cePin = 53;
+    const int _csPin = 48; 
+    */
+  
+    //RPI
+    /*
+    const int _cePin = 22;
+    const int _csPin = 0;
+    */
+    
+public:
+    char lastMessage[32];
+    
+    NRF24Radio(int, int);
+    
+    void broadcast(char *);
+    
+    char *listen();
+    char *listenFor(unsigned long);
+};
+
+#endif
